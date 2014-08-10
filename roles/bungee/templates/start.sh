599138922eba1673a4659817d06e63d64885621b
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-10 11:08:32 -0700

    Bungee running as a real service now.

--- roles/bungee/templates/start.sh
+++ roles/bungee/templates/start.sh
@@ -2,7 +2,4 @@
 
 # Dropped off by Ansible. If you modify this your changes will be OBLITERATED!    <----
 
-# This is a hack to get around the ">>>>" errors.
-# To connect, su {{username}} and run screen -x
-screen -dmS bungee-{{ port }} java -jar {{ installation_directory }}bungee.jar
-#screen -dmS bungee-{{ port }} su {{ username }} -c "java -jar {{ installation_directory }}bungee.jar"
+java -jar {{ installation_directory }}bungee.jar
