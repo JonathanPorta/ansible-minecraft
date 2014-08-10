cb19343c20c04b54ec9b1f542dbc8073c90a513f
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-09 19:06:10 -0700

    Initial Commit. Does bas config and secure of server and installs spigot.

---
+++ roles/spigot/templates/start.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Dropped off by Ansible. If you modify this your changes will be OBLITERATED!    <----
+
+java -Xms512M -Xmx1536M -XX:MaxPermSize=128M -jar {{ installation_directory }}spigot.jar
