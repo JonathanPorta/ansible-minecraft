20f134389b9c4b84f1d8c74930633c1688253680
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-09 22:03:14 -0700

    Spigot gets installed as a systemd service.

--- roles/spigot/templates/start.sh
+++ roles/spigot/templates/start.sh
@@ -2,4 +2,4 @@
 
 # Dropped off by Ansible. If you modify this your changes will be OBLITERATED!    <----
 
-java -Xms512M -Xmx1536M -XX:MaxPermSize=128M -jar {{ installation_directory }}spigot.jar
+java -Xms512M -Xmx1536M -XX:MaxPermSize=128M -jar {{ installation_directory }}spigot.jar --noconsole
