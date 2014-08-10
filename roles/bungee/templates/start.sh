de02058bcaffde21678bc11454da5298415be612
Jonathan Porta <jonathan@jonathanporta.com>
2014-08-10 00:32:20 -0700

    Yay, bungeecord works

--- roles/bungee/templates/start.sh
+++ roles/bungee/templates/start.sh
@@ -2,4 +2,7 @@
 
 # Dropped off by Ansible. If you modify this your changes will be OBLITERATED!    <----
 
-java -jar {{ installation_directory }}bungee.jar
+# This is a hack to get around the ">>>>" errors.
+# To connect, su {{username}} and run screen -x
+screen -dmS bungee-{{ port }} java -jar {{ installation_directory }}bungee.jar
+#screen -dmS bungee-{{ port }} su {{ username }} -c "java -jar {{ installation_directory }}bungee.jar"
